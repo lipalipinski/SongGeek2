56b56967c3c00ee35753e36a49eb25e8055678c5
JL <lipabass@gmail.com>
2023-01-26 23:01:07 +0100

    cache config longer timeouts

--- app/__init__.py
+++ app/__init__.py
@@ -32,8 +32,8 @@ if app.config["CACHE_SERVERS"] == None:
     cache.init_app(app)
 else:
     cache = Cache()
-    cache_user = os.environ.get('MEMCACHIER_USERNAME') or ''
-    cache_pass = os.environ.get('MEMCACHIER_PASSWORD') or ''
+    cache_user = os.environ.get('MEMCACHIER_USERNAME')
+    cache_pass = os.environ.get('MEMCACHIER_PASSWORD')
     cache.init_app(app, config={'CACHE_TYPE': 'SASLMemcachedCache',
                 'CACHE_MEMCACHED_SERVERS': app.config["CACHE_SERVERS"].split(','),
                 'CACHE_MEMCACHED_USERNAME': cache_user,
@@ -44,14 +44,14 @@ else:
                     # Keep connection alive
                     'tcp_keepalive': True,
                     # Timeout for set/get requests
-                    'connect_timeout': 2000, # ms
+                    'connect_timeout': 4000, # ms
                     'send_timeout': 750 * 1000, # us
                     'receive_timeout': 750 * 1000, # us
                     '_poll_timeout': 2000, # ms
                     # Better failover
                     'ketama': True,
                     'remove_failed': 1,
-                    'retry_timeout': 2,
+                    'retry_timeout': 4,
                     'dead_timeout': 30}}})
 
 # logging into files
