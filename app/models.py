6969cccbd9c871db162df429ecca234b68c5fbe2
JL <lipabass@gmail.com>
2023-01-26 23:04:17 +0100

    memoize comented out

--- app/models.py
+++ app/models.py
@@ -134,7 +134,7 @@ class User(UserMixin, db.Model):
 
         return top_tracks
 
-    @cache.memoize(timeout=60)
+    #@cache.memoize(timeout=60)
     def top_artists(self):
         """ returns [{'artst':plst, 'score':score}, ...] """
         # minimum number of plays
@@ -185,12 +185,12 @@ class User(UserMixin, db.Model):
         top_playlists.sort(key = lambda playlist : playlist["score"], reverse=True)
         return top_playlists
 
-    @cache.memoize(timeout=5)
+    #@cache.memoize(timeout=5)
     def count_games(self):
         games = db.session.query(Game).filter(Game.user_id == self.id, Game.status == 5).count()
         return games
 
-    @cache.memoize(timeout=5)
+    #@cache.memoize(timeout=5)
     def answers(self):
         """ returns (correct_answers, all_answers) """
         # consider only finished games (Game.status == 5)
@@ -468,7 +468,7 @@ class Playlist(db.Model):
         return statistics.mean([game.points() for game in user_games.all()])
 
 
-    @cache.memoize(timeout=60)
+    #@cache.memoize(timeout=60)
     def level(self):
 
         if self.avg_score() == 0:
