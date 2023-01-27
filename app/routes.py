95fca0902c14cb9cfda45ec6334547ef42e86b84
JL <lipabass@gmail.com>
2023-01-27 10:25:34 +0100

    args to ignore memoize plst fetch

--- app/routes.py
+++ app/routes.py
@@ -109,7 +109,7 @@ def require_country():
 @app.route("/", methods=["POST", "GET"])
 def index():
 
-    @cache.memoize(timeout=900)
+    @cache.memoize(timeout=900, args_to_ignore=["spoti"])
     @retryfy(3, 2)
     def fetch_playlists(spoti, limit, code = None):
         app.logger.debug("FETCH_PLAYLIST NOT CACHED")
