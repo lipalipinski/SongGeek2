ff65189a325b6ade83ed64db6cb01207e4e70f5f
JL <lipabass@gmail.com>
2023-01-27 10:48:36 +0100

    first country js value first ctry ok

--- app/static/featured.js
+++ app/static/featured.js
@@ -151,6 +151,7 @@ function changeCountry(code) {
         .then((json) => {
             // update page
             selectedCountry.textContent = `${json["code"]}: ${json["name"]}`;
+            selectCountry.setAttribute('value', json["code"]);
             buttonCountry.textContent = `Country: ${json["name"]}`;
         })
         .then(() => {
