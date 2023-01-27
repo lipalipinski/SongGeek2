baa655cc9f942c3bc3246643d0ae0b150691e22c
JL <lipabass@gmail.com>
2023-01-27 09:58:20 +0100

    logging debug

--- app/__init__.py
+++ app/__init__.py
@@ -39,7 +39,7 @@ if not app.debug and not app.testing:
         file_handler.setLevel(logging.DEBUG)
         app.logger.addHandler(file_handler)
 
-    app.logger.setLevel(logging.INFO)
+    app.logger.setLevel(logging.DEBUG)
     app.logger.info('SongGeek startup')
 
 
