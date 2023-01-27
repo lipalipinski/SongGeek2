6374972fd7368a8f73a0771fcdf65fd2a3f74449
JL <lipabass@gmail.com>
2023-01-27 09:36:37 +0100

    cache_config

--- app/__init__.py
+++ app/__init__.py
@@ -55,26 +55,26 @@ else:
     cache = Cache()
     cache_user = os.environ.get('MEMCACHIER_USERNAME') or ''
     cache_pass = os.environ.get('MEMCACHIER_PASSWORD') or ''
-    cache.init_app(app,
-        config={'CACHE_TYPE': 'saslmemcached',
-                'CACHE_MEMCACHED_SERVERS': app.config["CACHE_SERVERS"].split(','),
-                'CACHE_MEMCACHED_USERNAME': cache_user,
-                'CACHE_MEMCACHED_PASSWORD': cache_pass,
-                'CACHE_OPTIONS': { 'behaviors': {
-                    # Faster IO
-                    'tcp_nodelay': True,
-                    # Keep connection alive
-                    'tcp_keepalive': True,
-                    # Timeout for set/get requests
-                    'connect_timeout': 2000, # ms
-                    'send_timeout': 750 * 1000, # us
-                    'receive_timeout': 750 * 1000, # us
-                    '_poll_timeout': 2000, # ms
-                    # Better failover
-                    'ketama': True,
-                    'remove_failed': 1,
-                    'retry_timeout': 2,
-                    'dead_timeout': 30}}})
+    cache_config={'CACHE_TYPE': 'saslmemcached',
+            'CACHE_MEMCACHED_SERVERS': app.config["CACHE_SERVERS"].split(','),
+            'CACHE_MEMCACHED_USERNAME': cache_user,
+            'CACHE_MEMCACHED_PASSWORD': cache_pass,
+            'CACHE_OPTIONS': { 'behaviors': {
+                # Faster IO
+                'tcp_nodelay': True,
+                # Keep connection alive
+                'tcp_keepalive': True,
+                # Timeout for set/get requests
+                'connect_timeout': 2000, # ms
+                'send_timeout': 750 * 1000, # us
+                'receive_timeout': 750 * 1000, # us
+                '_poll_timeout': 2000, # ms
+                # Better failover
+                'ketama': True,
+                'remove_failed': 1,
+                'retry_timeout': 2,
+                'dead_timeout': 30}}}
+    cache.init_app(app, cache_config)
 
 # spotipy client credentials
 auth_manager = SpotifyClientCredentials()
